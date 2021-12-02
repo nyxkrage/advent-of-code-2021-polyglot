be913851151ea8934c78d8215016855a131403fb
Carsten Kragelund <carsten@kragelund.dev>
2021-12-02 19:59:03 +0100

    Day 2 in F#

--- 02-fsharp/Program.fs
+++ 02-fsharp/Program.fs
@@ -1,13 +1,38 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let TransformInput (input: string) = 
+    let words = input.Split(' ');
+    (words.[0], words.[1])
+
+let ReduceInstructions acc value =
+    let (distance, depth, aim) = acc;
+    let (forward, vertical,_) = value;
+    let newAim = aim + vertical;
+
+    (distance + forward, forward * newAim + depth, newAim)
+
+let part1 input =
+    let (f,d) = input |> Seq.reduce (fun a b -> (fst a + fst b, snd a + snd b));
+    f * d
+
+let part2 input =
+    let (f,d,_) = input |> Seq.map (fun a -> (fst a, snd a, 0)) |> Seq.reduce ReduceInstructions;
+    f * d
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let input = File.ReadAllText("input").Split("\n") |> Seq.map TransformInput |> Seq.map (fun ((command, value): string*string) -> 
+        match command with
+            | "forward" -> (value |> int, 0)
+            | "down" -> (0, value |> int)
+            | "up" -> (0, -(value |> int))
+        );
+    let result1 = input |> part1;
+    let result2 = input |> part2;
+
+    printf "%d\n" result1;
+    printf "%d\n" result2;
     0 // return an integer exit code
